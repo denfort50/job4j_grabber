91426178b8249cce557f24e50a774cceb2f81c7e
denfort50 <denfort50@yandex.ru>
2022-02-04 08:06:25 +0300

    2.1. Преобразование даты [#289476 #259729]

--- src/main/java/ru/job4j/grabber/utils/SqlRuDateTimeParser.java
+++ src/main/java/ru/job4j/grabber/utils/SqlRuDateTimeParser.java
@@ -6,6 +6,8 @@ import static java.util.Map.entry;
 
 public class SqlRuDateTimeParser implements DateTimeParser {
 
+    public static final LocalDate TODAY = LocalDate.now();
+    public static final LocalDate YESTERDAY = LocalDate.now().minus(Period.ofDays(1));
     private static final Map<String, Integer> MONTHS = Map.ofEntries(
             entry("янв", 1), entry("фев", 2), entry("мар", 3),
             entry("апр", 4), entry("май", 5), entry("июн", 6),
@@ -19,9 +21,9 @@ public class SqlRuDateTimeParser implements DateTimeParser {
         LocalTime localTime = LocalTime.of(Integer.parseInt(time[0]), Integer.parseInt(time[1]));
         LocalDateTime result;
         if ("сегодня".equals(dateAndTime[0])) {
-            result = LocalDateTime.of(LocalDate.now(), localTime);
+            result = LocalDateTime.of(TODAY, localTime);
         } else if ("вчера".equals(dateAndTime[0])) {
-            result = LocalDateTime.of(LocalDate.now().minus(Period.ofDays(1)), localTime);
+            result = LocalDateTime.of(YESTERDAY, localTime);
         } else {
             String[] date = dateAndTime[0].split(" ");
             int day = Integer.parseInt(date[0]);
