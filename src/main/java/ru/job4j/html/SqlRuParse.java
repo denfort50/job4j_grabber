b20fe57c48d8f45de3aa8cd5f403209a70101ebe
denfort50 <denfort50@yandex.ru>
2022-02-04 07:21:10 +0300

    2.1.1. Парсинг https://www.sql.ru/forum/job-offers/3 [#285210]

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -8,19 +8,23 @@ import ru.job4j.grabber.utils.SqlRuDateTimeParser;
 
 public class SqlRuParse {
     public static void main(String[] args) throws Exception {
-        Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
-        Elements row = doc.select(".postslisttopic");
-        for (Element td : row) {
-            Element href = td.child(0);
-            System.out.println(href.attr("href"));
-            System.out.println(href.text());
-            Element parent = td.parent();
-            assert parent != null;
-            Element altCol = parent.child(5);
-            SqlRuDateTimeParser parser = new SqlRuDateTimeParser();
-            System.out.println("Дата обновления поста (до преобразования): " + altCol.text());
-            System.out.println("Дата обновления поста (после преобразования): " + parser.parse(altCol.text())
-                    + System.lineSeparator());
+        int pageCounter = 1;
+        while (pageCounter <= 5) {
+            Document doc = Jsoup.connect(String.format("https://www.sql.ru/forum/job-offers/%d", pageCounter)).get();
+            Elements row = doc.select(".postslisttopic");
+            for (Element td : row) {
+                Element href = td.child(0);
+                System.out.println(href.attr("href"));
+                System.out.println(href.text());
+                Element parent = td.parent();
+                assert parent != null;
+                Element altCol = parent.child(5);
+                SqlRuDateTimeParser parser = new SqlRuDateTimeParser();
+                System.out.println("Дата обновления поста (до преобразования): " + altCol.text());
+                System.out.println("Дата обновления поста (после преобразования): " + parser.parse(altCol.text())
+                        + System.lineSeparator());
+            }
+            pageCounter++;
         }
     }
 }
