55798d397efc58caa5cd12b974f61e585d540f62
denfort50 <denfort50@yandex.ru>
2022-02-06 19:13:13 +0300

    6. Grabber. [#289477]

---
+++ src/main/java/ru/job4j/grabber/Grabber.java
@@ -0,0 +1,79 @@
+package ru.job4j.grabber;
+
+import org.quartz.*;
+import org.quartz.impl.StdSchedulerFactory;
+import ru.job4j.grabber.model.Post;
+import ru.job4j.grabber.utils.SqlRuDateTimeParser;
+import ru.job4j.html.SqlRuParse;
+
+import java.io.*;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Properties;
+
+import static org.quartz.JobBuilder.newJob;
+import static org.quartz.SimpleScheduleBuilder.simpleSchedule;
+import static org.quartz.TriggerBuilder.newTrigger;
+
+public class Grabber implements Grab {
+    private final Properties cfg = new Properties();
+
+    public Store store() throws SQLException {
+        return new PsqlStore(cfg);
+    }
+
+    public Scheduler scheduler() throws SchedulerException {
+        Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
+        scheduler.start();
+        return scheduler;
+    }
+
+    public void cfg() throws IOException {
+        try (InputStream in =
+                     new FileInputStream("C:/projects/job4j_grabber/src/main/resources/app.properties")) {
+            cfg.load(in);
+        }
+    }
+
+    @Override
+    public void init(Parse parse, Store store, Scheduler scheduler) throws SchedulerException {
+        JobDataMap data = new JobDataMap();
+        data.put("store", store);
+        data.put("parse", parse);
+        JobDetail job = newJob(GrabJob.class)
+                .usingJobData(data)
+                .build();
+        SimpleScheduleBuilder times = simpleSchedule()
+                .withIntervalInSeconds(Integer.parseInt(cfg.getProperty("time")))
+                .repeatForever();
+        Trigger trigger = newTrigger()
+                .startNow()
+                .withSchedule(times)
+                .build();
+        scheduler.scheduleJob(job, trigger);
+    }
+
+    public static class GrabJob implements Job {
+
+        @Override
+        public void execute(JobExecutionContext context) {
+            JobDataMap map = context.getJobDetail().getJobDataMap();
+            Store store = (Store) map.get("store");
+            Parse parse = (Parse) map.get("parse");
+            String pageLink = "https://www.sql.ru/forum/job-offers";
+            List<Post> postList = parse.list(pageLink);
+            postList.forEach(store::save);
+            System.out.println("Парсинг вакансий для java-разработчиков с сайта sql.ru завершён.");
+            System.out.println("Количество вакансий в базе данных: " + store.getAll().size());
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Grabber grab = new Grabber();
+        grab.cfg();
+        Scheduler scheduler = grab.scheduler();
+        Store store = grab.store();
+        SqlRuDateTimeParser parser = new SqlRuDateTimeParser();
+        grab.init(new SqlRuParse(parser), store, scheduler);
+    }
+}
